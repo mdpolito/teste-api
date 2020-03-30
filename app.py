61d6703ff5fd18502fe4c38f5a9cf8b5ac5f9208
zcm4 <marcellopolito@br-petrobras.com.br>
2020-03-30 15:32:12 -0300

    primeira versao

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, abort, make_response
+import socket
+
+app = Flask(__name__)
+
+
+@app.route('/produtos', methods=['GET'])
+def produto():
+
+    try:
+
+        lista = ({"codigo": "000001", "nome": "Gasolina comum"},
+                 {"codigo": "000002", "nome": "Gasolina aditivada"},
+                 {"codigo": "000003", "nome": "Etanol"},
+                 {"codigo": "000004", "nome": "Oleo diesel"})
+
+        return jsonify(lista)
+
+    except Exception as erro:
+        abort(500, 'Ocorreu um erro interno: ' + str(erro))
+
+
+@app.errorhandler(500)
+def erro500(error):
+    response = jsonify({'mensagem': error.description})
+    return make_response(response, 500)
+
+
+# Recupera o endereço IP do servidor de aplicação
+hostIP = socket.gethostbyname(socket.gethostname())
+
+# Executa a aplicação
+app.run(host=hostIP, port=5000, debug=False)
